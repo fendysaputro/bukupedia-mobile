ea59f88334a6d147a59a4600b5bacb3b1be60f64
Fendy Saputro <fendy@amg.co.id>
2018-04-15 09:36:37 +0700

    update Account js

--- components/Account.js
+++ components/Account.js
@@ -1,38 +1,105 @@
 import React, { Component } from "react";
-import { AppRegistry, StyleSheet, Text, View, Image, Search } from "react-native";
+import { AppRegistry, StyleSheet, Text, View, Image, Search, TouchableOpacity, Button, List } from "react-native";
 import { COLOR_PRIMARY } from "../styles/common";
 import TabNavigator from "react-native-tab-navigator";
-import BottomNavigation, { Tab } from "react-native-material-bottom-navigation";
+import { ListItem } from "react-native-elements";
 
 export default class Account extends Component {
   static navigationOptions = {
+    title: 'Akun',
     headerStyle: {
       backgroundColor: COLOR_PRIMARY,
-      elevation: null
+      elevation: null,
     },
-    headerLeft: null,
+    headerTitleStyle: {
+      color: 'white',
+      width: '90%',
+      textAlign: 'center'
+    },
+    headerLeft: null
   }
+
+  stateOne = {
+    listOne: [
+      {
+        id: 0,
+        name: 'Hi, Selamat Datang!',
+      }
+    ]
+  }
+  stateTwo = {
+    listTwo: [
+      {
+        id: 0,
+        title: 'Rating Aplikasi'
+      },
+      {
+        id: 1,
+        title: 'Blog Kami'
+      },
+      {
+        id: 2,
+        title: 'Bantuan                                                                                   >'
+      }
+    ]
+  }
+
   render () {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          This is Account
-          ini adalah akun
-        </Text>
+      <View>
+        {
+          this.stateOne.listOne.map((itemOne, index) => (
+            <TouchableOpacity
+              key = {itemOne.id}
+              style = {styles.containerOne}
+            >
+              <Text style = {styles.text}>
+                {itemOne.name}
+              </Text>
+            </TouchableOpacity>
+          ))
+        }
+        {
+          this.stateTwo.listTwo.map((itemTwo, index) => (
+            <TouchableOpacity
+              key = {itemTwo.id}
+              style = {styles.containerTwo}
+            >
+              <Text style = {styles.text}>
+                {itemTwo.title}
+              </Text>
+            </TouchableOpacity>
+          ))
+        }
       </View>
     )
   }
 }
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+  containerOne: {
+    padding: 10,
+    marginBottom: 15, 
+    backgroundColor: 'white'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
+  containerTwo: {
+    padding: 10,
+    marginTop: 3,
+    backgroundColor: 'white' 
+  },
+  text: {
+    color: 'black',
+    textAlign: 'left'
+  },
+  symbol:{
+    color: 'black',
+    textAlign: 'right'
+  },
+  button: {
+    color: 'yellow'
+  },
+  buttonText: {
+    color: 'black',
+    textAlign: 'center'
   }
 });
 
