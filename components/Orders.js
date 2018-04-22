3b04cfa5ff2db50434f6d8e412f2c234503b188e
Fendy Saputro <fendy@amg.co.id>
2018-04-22 11:04:42 +0700

    update orders.js

--- components/Orders.js
+++ components/Orders.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { AppRegistry, StyleSheet, Text, View, Image, Search } from "react-native";
+import { AppRegistry, StyleSheet, Text, View, Image, Search, TextInput, TouchableOpacity } from "react-native";
 import { COLOR_PRIMARY } from "../styles/common";
 import TabNavigator from "react-native-tab-navigator";
 import BottomNavigation, { Tab } from "react-native-material-bottom-navigation";
@@ -36,7 +36,7 @@ export default class Orders extends Component {
             color: 'black', 
             backgroundColor: '#FBAD19',
             alignSelf: "center",
-            width: 275,
+            width: 290,
             height: 35,
             alignItems: 'center',
             justifyContent: 'center',
@@ -46,6 +46,29 @@ export default class Orders extends Component {
           onPress = {() => this.alertItemName("Masuk")}>
             Masuk
         </Button>
+        <Text style={styles.lineText}>
+          ─────────────  Atau  ─────────────
+        </Text>
+        <Text style={styles.textLogin}>
+          Email
+        </Text>
+        <TextInput style={styles.input}
+          placeHolder = "Email"
+          underlineColorAndroid = "transparent"
+        >
+        </TextInput>
+        <Text style={styles.textLogin}>
+          Nomor Invoice
+        </Text>
+        <TextInput style={styles.input}
+          placeHolder = "Invoice"
+          underlineColorAndroid = "transparent"
+        >
+        </TextInput>
+        <TouchableOpacity
+          style = {styles.submitButton}>
+          <Text style = {styles.submitButtonText}> Cari </Text>
+        </TouchableOpacity>
       </View>
     )
   }
@@ -58,9 +81,40 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 15,
     textAlign: "center",
-    marginTop: "15%",
+    marginTop: "15%"
+  },
+  lineText: {
+    fontSize: 13,
+    textAlign: "center",
+    marginTop: "5%"
+  },
+  textLogin: {
+    fontSize: 15,
+    textAlign: "left",
+    alignSelf: "flex-start",
+    marginLeft: "10%",
+    marginTop: "1%",
+    paddingTop: 10,
     fontWeight: "bold"
   },
+  input: {
+    height: 30,
+    width: 285,
+    alignSelf: "flex-start",
+    marginLeft: "10%",
+    backgroundColor: "#D3D3D3"
+  },
+  submitButton: {
+    backgroundColor: COLOR_PRIMARY,
+    padding: 10,
+    margin: 15,
+    height: 35,
+    width: 290
+ },
+ submitButtonText:{
+    color: "white",
+    textAlign: "center"
+ }
 });
 
 AppRegistry.registerComponent("Orders", () => Orders);
