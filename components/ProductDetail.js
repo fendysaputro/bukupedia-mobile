6d6a19fac5a4c89ccc8fc6c44414729293ef20e0
Fendy Saputro <fendy@amg.co.id>
2018-06-01 01:29:22 +0700

    update

--- components/ProductDetail.js
+++ components/ProductDetail.js
@@ -1,11 +1,36 @@
 import React, { Component } from "react";
 import {  AppRegistry,
 		  Text, 
-          View } from "react-native";
+          View, Image, TouchableOpacity, Button } from "react-native";
 import { COLOR_PRIMARY, sliderWidth, itemWidth } from "../styles/common";
 
 export default class ProductDetail extends Component {
-
+    static navigationOptions = ({navigation}) => ({
+        title: "Detail Produk",
+        headerStyle: {
+          backgroundColor: COLOR_PRIMARY,
+          elevation: null
+        },
+        headerTitleStyle: {
+          width: '90%',
+          textAlign: 'center',
+          color: 'white'
+        },
+        headerRight:
+            <TouchableOpacity onPress={() => navigation.navigate("Login")}
+                style={{marginTop:2, width:24, height:24}}>
+                <Image
+                    source={require('../styles/icon/menu.png')}
+                />
+            </TouchableOpacity>,
+        Button:
+            <TouchableOpacity onPress={() => navigation.navigate("Login")}
+                style={{marginTop:2, marginLeft:10, width:24, height:24}}>
+                <Image
+                    source={require('../styles/icon/searchhh.png')}
+                />
+            </TouchableOpacity>
+    })
 	render() {
 		const { navigation } = this.props;
     	const url = navigation.getParam('url', '');
@@ -16,7 +41,6 @@ export default class ProductDetail extends Component {
 			</View>
 		)
 	}
-
 }
 
 AppRegistry.registerComponent("ProductDetail", () => ProductDetail);
